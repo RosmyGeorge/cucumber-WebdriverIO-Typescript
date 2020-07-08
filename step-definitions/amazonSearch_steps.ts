c725cfd0cf606b373e447bf8a129fce91ca1e223
RosmyGeorge <rosmygeorge8@gmail.com>
2020-07-09 00:11:58 +0100

    Create amazonSearch_steps.ts

---
+++ step-definitions/amazonSearch_steps.ts
@@ -0,0 +1,16 @@
+import { Given,When,Then } from 'cucumber'
+import {homepage} from 'support/amazon-home-page'
+
+    
+    Given('I navigate to amazon home page', function () {
+        browser.deleteCookies()
+        homepage.navigate()
+    })
+
+    When('I search for Green', function () {
+       homepage.searchForColor()
+    })
+
+    Then('I get relevant information', function () {
+       expect(homepage.colorSearchResults).toBePresent
+    })
