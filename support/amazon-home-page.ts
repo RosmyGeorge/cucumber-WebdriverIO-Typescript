c4361dd3be4157f5b86c406942cacd89a14af451
RosmyGeorge <rosmygeorge8@gmail.com>
2020-07-10 00:36:47 +0100

    removed unused locator

--- support/amazon-home-page.ts
+++ support/amazon-home-page.ts
@@ -2,7 +2,6 @@ export class AmazonHomePage{
 
     get searchbox(){ return $('input[name=field-keywords]')}
     get submitsearch(){ return $('input[class=nav-input]')}
-    get colorSearchResults(){return $('span[class= a-color-state]')}
 
     navigate(){
         browser.navigateTo('https://www.amazon.co.uk/')}
